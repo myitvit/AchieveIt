2a654228d0829887560394a78c8bf9ef30d5dad5
Vitali Prakapenka <myitvit@gmail.com>
2022-11-19 12:35:32 +0300

    Recreated API project and updated to .NET 7

--- AchieveIt.API/Controllers/WeatherForecastController.cs
+++ AchieveIt.API/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace AchieveIt.Controllers
+namespace AchieveIt.API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
@@ -23,7 +23,7 @@ namespace AchieveIt.Controllers
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateTime.Now.AddDays(index),
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
